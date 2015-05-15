d7c2a6efad027d6c428825342588186c105317a6
carlospiad <carlospiad@gmail.com>
2015-05-15 13:18:09 -0500

    functional update

--- statement_parse.py
+++ statement_parse.py
@@ -1,10 +1,13 @@
 import csv
 
-with open('data/000101027265_0415_20150514164017.csv', newline='') as csvfile:
-  with open('data/metrozoho.csv', 'w+') as csvout:
+with open('data/convertcsv.csv', newline='') as csvfile:
+  with open('data/metrovisazoho.csv', 'w+') as csvout:
     writer = csv.writer(csvout, lineterminator='\n')
     cs = csv.reader(csvfile, delimiter=',', quotechar='"')
     next(cs, None)  # skip the first row from the reader, the old header
     writer.writerow(['Date-dd/MM/YY', 'Withdrawals', 'Deposits', 'Payee', 'Description', 'Reference Number']) # write new header
     for line in cs:
-        writer.writerow((line[4],line[8],line[9],'',line[5],line[6]))
+        if line[0] == '':
+            print (''.join(line) +' was eliminated')
+        else:
+            writer.writerow((line[0],line[4],line[5],'',line[3],line[2]))
