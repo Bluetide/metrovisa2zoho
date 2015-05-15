7f0eec2b81908c8dd336a8e5ee6d8233e487876e
carlospiad <carlospiad@gmail.com>
2015-05-15 14:05:21 -0500

    update to fix date string

--- statement_parse.py
+++ statement_parse.py
@@ -5,9 +5,9 @@ with open('data/convertcsv.csv', newline='') as csvfile:
     writer = csv.writer(csvout, lineterminator='\n')
     cs = csv.reader(csvfile, delimiter=',', quotechar='"')
     next(cs, None)  # skip the first row from the reader, the old header
-    writer.writerow(['Date-dd/MM/YY', 'Withdrawals', 'Deposits', 'Payee', 'Description', 'Reference Number']) # write new header
+    writer.writerow(['Date-MM/dd/YY', 'Withdrawals', 'Deposits', 'Payee', 'Description', 'Reference Number']) # write new header
     for line in cs:
         if line[0] == '':
             print (''.join(line) +' was eliminated')
         else:
-            writer.writerow((line[0],line[4],line[5],'',line[3],line[2]))
+            writer.writerow((line[1],line[4][1:],line[5][1:],'',line[3],line[2]))
